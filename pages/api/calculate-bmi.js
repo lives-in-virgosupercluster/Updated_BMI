94584aefb227e64070d61c761346f98361f1f510
Harsh <harsh.singh0310@gmail.com>
2024-02-15 23:38:31 +0530

    changed restapi method from post to get

--- pages/api/calculate-bmi.js
+++ pages/api/calculate-bmi.js
@@ -1,54 +1,50 @@
 export default function handler(req, res) {
    const {method}=req;
-   if(method==="POST"){
-    const {weight,height,weightUnit,unit}=req.body;
-    let finweight=0;
-    let finheight=0;
-    if(weightUnit==='pounds'){
-        finweight=0.453592*weight;
+   // why get method?
+   //The main use case for the GET method is to retrieve data from a server without changing the state of the server or the data on it
+   if(method==="GET"){
+    const { weight, height, weightUnit, unit } = req.query;
 
+    let finweight = 0;
+    let finheight = 0;
+
+    if (weightUnit === 'pounds') {
+      finweight = 0.453592 * parseFloat(weight);
+    } else if (weightUnit === 'kilograms') {
+      finweight = parseFloat(weight);
     }
-    if(weightUnit==='kilograms'){
-        finweight=weight;
+
+    // Parse the height back to an object
+    const parsedHeight = JSON.parse(height);
+
+    if (unit === 'feet') {
+      finheight = parsedHeight.feet * 0.3048;
+      finheight += parsedHeight.inches * 0.0254;
+    } else if (unit === 'cm') {
+      finheight = parsedHeight.cm * 0.01;
     }
-    if(unit==='feet'){
-        finheight=height.feet*0.3048;
-        finheight+=height.inches*0.0254;
-    }
-    if(unit==='cm'){
-        finheight=height.cm*0.01;
-    }
-  
-    let bmivalue=finweight/(finheight*finheight);
+
+    let bmivalue = finweight / (finheight * finheight);
     let category;
     let upper;
     let lower;
-    if(bmivalue<18.5){
-      category="UnderWeight";
-    }
-    else if(bmivalue>=18.5 && bmivalue<=24.9){
-      category="Healthy"
-   
-    }
-    else if(bmivalue>=24.9 && bmivalue<=29.9){
-      category="OverWeight";
-     
-      
-    }
-    else if(bmivalue>29.9){
-      category="Obese";
-      
-    }
-    //console.log(bmi);
-    
-    lower=18.5*(finheight)*(finheight);
-    upper=24.9*(finheight)*(finheight);
-    lower=lower.toFixed(0);
-    upper=upper.toFixed(0);
-    console.log(lower);
-   
 
-    res.status(200).json({bmi:Number(bmivalue.toFixed(2)),weight:finweight,height:finheight,category,lower,upper});
+    if (bmivalue < 18.5) {
+      category = "UnderWeight";
+    } else if (bmivalue >= 18.5 && bmivalue <= 24.9) {
+      category = "Healthy";
+    } else if (bmivalue >= 24.9 && bmivalue <= 29.9) {
+      category = "OverWeight";
+    } else if (bmivalue > 29.9) {
+      category = "Obese";
+    }
+
+    lower = 18.5 * (finheight) * (finheight);
+    upper = 24.9 * (finheight) * (finheight);
+    lower = lower.toFixed(0);
+    upper = upper.toFixed(0);
+
+    res.status(200).json({ bmi: Number(bmivalue.toFixed(2)), weight: finweight, height: finheight, category, lower, upper });
    }
   }
   
